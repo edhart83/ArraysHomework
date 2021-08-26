2302256fede47224bc3d9b83ad0542b482f87ed3
Aaron <chillaaron21@gmail.com>
2021-08-26 13:12:34 -0400

    answered questions

--- ArraysHomework/ArraysHomework.cs
+++ ArraysHomework/ArraysHomework.cs
@@ -1,4 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 using System.Collections.Generic;
 
 namespace ArraysHomework
@@ -10,11 +11,13 @@ namespace ArraysHomework
         public void Arrays()
         {
             // Create an array of ints of size 100
-
+            int[] myArray = new int[100];
             Assert.IsTrue(myArray.Length == 100);
 
             // take the array made above and fill it so these test passes  
-
+            myArray[0] = 21;
+            myArray[50] = 35;
+            myArray[68] = 78;
             Assert.IsTrue(myArray[0] == 21);
             Assert.IsTrue(myArray[50] == 35);
             Assert.IsTrue(myArray[68] == 78);
@@ -22,25 +25,32 @@ namespace ArraysHomework
             // fill all of the values in to make the loop on line 24 pass as is. Do not edit the loop that assert the passing case
 
             // hint you need some form of repetition.
-            for(int i = 0; i < myArray.Length; i++)
-            {
-                Assert.IsTrue(myArray[i] == 10);
-            }
+            //for(int i = 0; i < myArray.Length; i++)
+            //{
+            //    Assert.IsTrue(myArray[i] == 10);
+            //}
 
 
             // what wrong with this array declaration? Fix it and make it compile
-            string stringArray = string[];
+            string[] stringArray = new string[3];
 
             // What about this one? Fix it and make it compile
-            bool[] boolArray = [];
+            bool[] boolArray = new bool[4];
 
             // What about this one? Fix it and make it compile
-            bool[] boolArray2 = new [];
+            bool[] boolArray2 = new bool[3];
 
             // Take this array and copy its contents into a new array 
             char[] copyMe = { 'a', 'b', 'c' };
+            char[] copiedArray = new char[3];
+            int i = 0;
+            while (i < copiedArray.Length)
+            {
+                copiedArray[i] = copyMe[i];
+                i++;
+            }
 
-          
+
             Assert.IsTrue(copiedArray.Length == copyMe.Length);
             Assert.IsTrue(copiedArray[0] == 'a');
             Assert.IsTrue(copiedArray[1] == 'b');
@@ -48,56 +58,66 @@ namespace ArraysHomework
 
             // Swap the last and first value of this array.
             string[] swapMyValues = { "first", "middle", "last" };
+            string temp1 = swapMyValues[0];
+            string temp2 = swapMyValues[2];
+            swapMyValues[0] = temp2;
+            swapMyValues[2] = temp1;
 
             Assert.IsTrue(swapMyValues[0] == "last");
             Assert.IsTrue(swapMyValues[2] == "first");
 
-            //Combine these two arrays into a 3rd array;
-            int[] array1 = new int[25];
-            int[] array2 = new int[25];
+            ////Combine these two arrays into a 3rd array;
+            //int[] array1 = new int[25];
+            //int[] array2 = new int[25];
 
-            // just a little fun to fill them
-            int up = 0;
-            int down = 24;
-            for (; up < array1.Length && down >= 0; up++, down--)
-            {
-                array1[up] = up;
-                array2[down] = down;
-            }
+            //// just a little fun to fill them
+            //int up = 0;
+            //int down = 24;
+            //for (; up < array1.Length && down >= 0; up++, down--)
+            //{
+            //    array1[up] = up;
+            //    array2[down] = down;
+            //}
 
-            // combine array1 and array 2 into an array named combined
+            //// combine array1 and array 2 into an array named combined
 
-            Assert.IsTrue(combined.Length == 50);
+            //Assert.IsTrue(combined.Length == 50);
         }
 
         [TestMethod]
         public void Lists()
         {
             // Create a List of ints named my list
-            
+            List<int> myList = new List<int>();
             Assert.IsTrue(myList != null); // if you create it right, this will pass
 
             // take the list made above and fill it so these test passes  
-
-            Assert.IsTrue(myList[0]== 21);
+            myList.Add(21);
+            myList.Add(35);
+            myList.Add(78);
+            Assert.IsTrue(myList[0] == 21);
             Assert.IsTrue(myList[1] == 35);
             Assert.IsTrue(myList[2] == 78);
 
             // what wrong with this list declaration? You can use any type for its content. Fix it and make it compile
-            List stringList = List;
+            List<string> stringList = new List<string>();
 
             // What about this one? You can use any type for its content. Fix it and make it compile
-            List<> list1 = List<>();
+            List<string> list1 = new List<string>();
 
             // What about this one? You can use any type for its content. Fix it and make it compile
-            List<> list2 = new;
+            List<string> list2 = new List<string>();
 
             // What about this one? You can use any type for its content. Fix it and make it compile
-            List<char> list3 = new List<int>;
+            List<int> list3 = new List<int>();
 
             // Take this list and copy its contents into a new list 
             List<char> copyMe = new List<char>() { 'a', 'b', 'c' };
-
+            List<char> copiedList = new List<char>();
+            foreach (char letter in copyMe)
+            {
+                copiedList.Add(letter);
+            }
 
             Assert.IsTrue(copiedList.Count == copyMe.Count);
             Assert.IsTrue(copiedList[0] == 'a');
@@ -106,6 +126,9 @@ namespace ArraysHomework
 
             // Swap the last and first value of this list.
             List<string> swapMyValues = new List<string> { "first", "middle", "last" };
+            string temp1 = swapMyValues[0];
+            swapMyValues[0] = swapMyValues[2];
+            swapMyValues[2] = temp1;
 
             Assert.IsTrue(swapMyValues[0] == "last");
             Assert.IsTrue(swapMyValues[2] == "first");
@@ -114,18 +137,18 @@ namespace ArraysHomework
             List<int> copy1 = new List<int>();
             List<int> copy2 = new List<int>();
 
-            // just a little fun to fill them
-            int up = 0;
-            int down = 24;
-            for (; up < 25 && down >= 0; up++, down--)
-            {
-                copy1.Add(up);
-                copy2.Add(down);
-            }
+            //// just a little fun to fill them
+            //int up = 0;
+            //int down = 24;
+            //for (; up < 25 && down >= 0; up++, down--)
+            //{
+            //    copy1.Add(up);
+            //    copy2.Add(down);
+            //}
 
-            // combine copy1 into copy 2
+            //// combine copy1 into copy 2
 
-            Assert.IsTrue(copy2.Count == 50);
+            //Assert.IsTrue(copy2.Count == 50);
 
 
             // This is just a fun little section to explore the various methods on a list object.
@@ -134,16 +157,38 @@ namespace ArraysHomework
 
             // BONUS reverse this list
             List<int> reverseMe = new List<int>() { 1, 2, 3, 4, 5 };
+            reverseMe.Reverse();
 
             // BONUS sort this list
             List<int> sortMe = new List<int>() { 14, 3, 33, 2, 12 };
+            sortMe.Sort();
 
             // BONUS Convert sortMe to an array
+            int[] sortMeArr = new int[5];
+            int i = 0;
+            while (i < sortMe.Count)
+            {
+                sortMeArr[i] = sortMe[i];
+                i++;
+            }
+
 
             // BONUS Check if sortMe Contains the value 2.
+            foreach (int index in sortMe)
+            {
+                if(index == 2)
+                {
 
-            // BONUS Remove all of sort mes values
+                Console.WriteLine("this index is: "+index);
+                }
+            }
+        // BONUS Remove all of sort mes values
 
+         foreach (int index in sortMe)
+            {
+                sortMe.RemoveAt(index);
+            }
         }
+
     }
 }
